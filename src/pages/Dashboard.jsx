c2634c8c00a2100b143301ba84526137c0800cba
Rahul Sarma <rahul.sarma.apps@gmail.com>
2026-07-04 17:00:35 +0530

    Tailwind css configured

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <h1 className="font-bold underline">This is Dashboard Component!</h1>
+    </>
+  );
+};
+
+export default Dashboard;
